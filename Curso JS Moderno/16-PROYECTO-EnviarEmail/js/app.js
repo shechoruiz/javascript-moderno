569afde1642511e27cf4757a5874c2071c48c642
Sergio Ruiz <shecho27@gmail.com>
2021-02-22 23:00:52 -0500

    Avance de la 16va sección: Etapa 3

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -2,6 +2,8 @@
 
 // Variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail')
+
 const email = document.querySelector('#email')
 const asunto = document.querySelector('#asunto')
 const mensaje = document.querySelector('#mensaje')
@@ -29,7 +31,21 @@ function validarFormulario(e){
   if(e.target.value.length > 0){
     console.log('Si hay algo')
   } else {
-    e.target.style.borderBottomColor = 'red'
     e.target.classList.add('border', 'border-red-500')
+    mostrarError()
   }
 }
+
+// Etapa 3: Limpiar los mensajes de error
+function mostrarError() {
+  const mensajeError = document.createElement('p')
+  mensajeError.textContent = 'Todos los campos son obligatorios'
+  mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error')
+
+  const errores = document.querySelectorAll('.error')
+  if(errores.length === 0 ){
+    formulario.appendChild(mensajeError)
+  }
+}
+
+
