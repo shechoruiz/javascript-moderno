1d23e16540e88f50d6d9a95c5a80056fefbfe28c
Sergio Ruiz <shecho27@gmail.com>
2021-05-24 21:51:35 -0500

    Corrección de texto

--- Curso JS Moderno/03-Strings/js/06-app.js
+++ Curso JS Moderno/03-Strings/js/06-app.js
@@ -5,5 +5,5 @@ const producto = 'Monitor de 20 pulgadas'
 // Repeticion de una cadena de texto
 console.log(producto.repeat(3))
 
-// Dividir un string
+// Dividir un string -> Convirtiendolo en un array
 console.log(producto.split(" ")) //Separa por el espacio en este ejemplo
